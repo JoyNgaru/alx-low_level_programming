8919cced174cefe88d24fd44f842d0f2787f3e3b
JoyNgaru <joywanjikum@gmail.com>
2023-03-16 09:36:04 -0700

    Task 5:  a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ * main - Write a C program using printf
  *
  * Return: 0
  */
