bbc35f7ba25fd0127d23063dad4de77b96ecc395
JoyNgaru <joywanjikum@gmail.com>
2023-03-16 13:07:13 -0700

    Task 8: A C program that prints a sentence to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
 /**
- * main A C program that print something followed by a new line, to the standard error
+ * main  C program to print somethin to the standard error
  * return: 1
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
-	return(1);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+	return (1);
 }
