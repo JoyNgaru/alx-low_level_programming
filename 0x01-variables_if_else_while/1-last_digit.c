31797410605e60a377280dcedec4093aff18f85a
JoyNgaru <joywanjikum@gmail.com>
2023-03-17 07:52:35 -0700

    Task 1: Giving value to the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,9 +16,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	lastnumber = n % 10;
-	printf("Last digit of %i is %i ", n, lastnumber);
+	printf("Last digit of %i is %i", n, lastnumber);
 	if (lastnumber > 5)
-		printf("and is greater than 5");
+		printf("and is greater than 5\n");
 	else if (lastnumber == 0)
 		printf("and is 0\n");
 	else
