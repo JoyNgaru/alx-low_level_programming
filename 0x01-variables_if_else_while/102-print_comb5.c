a5228627f8c36014732da5f23192de53173aa782
JoyNgaru <joywanjikum@gmail.com>
2023-03-17 14:20:51 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - List two digit numbers from 0 to 99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 100; num++)
+	{
+		for (num2 = 0; num2 < 100; num2++)
+		{
+			if (num1 < num2)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+				if (num1 != 98 || num2 != 99)
+				{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
