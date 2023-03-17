969247e9f713d42d12d44c0c344a2f55d18284cf
JoyNgaru <joywanjikum@gmail.com>
2023-03-17 11:44:16 -0700

    Task 5: Listing numbers from zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - List decimal numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num++)
+		printf(num);
+	printf('\n');
+	return (0);
+}
