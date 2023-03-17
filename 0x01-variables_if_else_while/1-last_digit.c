a1fdf334fc90327437dbcf03775b177abdebc3a4
JoyNgaru <joywanjikum@gmail.com>
2023-03-17 08:21:39 -0700

    Task 1: Assign value to the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,6 @@ int main(void)
 	else if (lastnumber == 0)
 		printf("and is 0\n");
 	else
-		printf("and is less than 6 and is not 0\n");
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
