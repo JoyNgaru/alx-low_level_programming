d434a8df8dbe47403b5a3aa35403cb1cda53310e
JoyNgaru <joywanjikum@gmail.com>
2023-03-17 12:34:48 -0700

    Task 6: list numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print numbers using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10 num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	Return(0);
+}
