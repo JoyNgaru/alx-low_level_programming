cd51c16493f1c9581275a1749b394e32d7f205c2
JoyNgaru <joywanjikum@gmail.com>
2023-03-17 05:55:37 -0700

    Task 1: a script that compiles a C file but does not link

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	lastnumber = n % 10;
-	printf("Last digit of %d is %d", n, lastnumber);
+	printf("Last digit of %i is %i", n, lastnumber);
 	if (lastnumber > 5)
 		printf("and is greater than 5\n");
 	else if (lastnumber == 0)
-		printf("and is 0")
+		printf("and is 0\n")
 	else
-		printf("and is less than 6 and is not 0");
+		printf("and is less than 6 and is not 0\n");
 	return (0);
 }
