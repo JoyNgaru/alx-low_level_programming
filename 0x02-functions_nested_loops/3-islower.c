8bf92970cc5908d696939d8e4cc9715218a0ab52
JoyNgaru <joywanjikum@gmail.com>
2023-03-21 03:08:27 -0700

    Task 3: Check if letter is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include main.h
+/**
+ *  _islower - identify is c is lowercase
+ * Return: 1 if char is lowercase, else, 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
