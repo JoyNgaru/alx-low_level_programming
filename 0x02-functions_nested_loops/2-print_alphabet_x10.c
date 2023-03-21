8a1ef738d6b64fbd9dc169fe32746197254cc69f
JoyNgaru <joywanjikum@gmail.com>
2023-03-21 02:54:17 -0700

    Task 2: print alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char alpha;
 
-	while (counting++ <= 9)
+	while (count++ <= 9)
 	{
-		for (alpha = 'a' alpha <= 'z'; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
-		_puchar('\n');
+		_putchar('\n');
 	}
 }
 
