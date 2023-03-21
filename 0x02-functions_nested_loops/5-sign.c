9198083432677a0bf18ab032c79e0085952c2183
JoyNgaru <joywanjikum@gmail.com>
2023-03-21 04:43:37 -0700

    Task 5: Assign signs to numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - we print the sign of a number
+ * @n: is the test number
+ * Return: 1 if its positive, 0 if its zero, and 0 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
+
