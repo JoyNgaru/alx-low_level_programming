00fee4d984c84b5d2019f5dacde059a654021466
JoyNgaru <joywanjikum@gmail.com>
2023-03-21 01:59:54 -0700

    Task 1: print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_puchar('\n');
+	return (0);
+}
