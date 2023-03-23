951e6847f67ebc05eb3f99cc6a6481d87284c920
JoyNgaru <joywanjikum@gmail.com>
2023-03-23 03:28:45 -0700

    Task 4: List numbers minus 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
- * print_most_numbers - list number 0 to 9 minus 2 and 4
- * return: void
- */
+*print_most_numbers - prints numbers 0 to 9 minus 2 and 4
+*/
+
 void print_most_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
-		_putchar(c);
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
