bcd8fec9659f02538157171e692b6d2a6a397d73
JoyNgaru <joywanjikum@gmail.com>
2023-03-23 05:37:09 -0700

    Task 9: FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print a fizzbuzz 1 to 100
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	{
+		if (num % 15 == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		if (num < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
