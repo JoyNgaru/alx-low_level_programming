e5dbb5c422a3528f91806df81d49ce39be0643c4
JoyNgaru <joywanjikum@gmail.com>
2023-03-23 01:37:19 -0700

    Task 5: Print 14 lines of numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - list numbers 0 to 14 ten times
+ */
+void more_numbers(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 1; num1 <= 10; num1++)
+	{
+		for (num2 = 0; num <= 14; num2++)
+		{
+			if (num2 >= 10)
+				_putchar('1');
+			_putchar (num2 % 10 + '0');
+		}
+		_puthcar('\n');
+	}
+}
