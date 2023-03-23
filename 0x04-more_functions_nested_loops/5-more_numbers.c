19eaf63fb4d33d837b7c623a7f1043cff17d91ec
JoyNgaru <joywanjikum@gmail.com>
2023-03-23 04:37:16 -0700

    Task5: print 14 numbers ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@
 void more_numbers(void)
 {
 
-int i, j;
-	for (i = 1; i <= 10; i++)
+int num1, num2;
+	for (num1 = 1; num1i <= 10; num1++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (num2 = 0; num2 <= 14; num2++)
 		{
-			if (j >= 10)
+			if (num2 >= 10)
 			_putchar('1');
-			_putchar(j % 10 + '0');
+			_putchar(num2 % 10 + '0');
 		}
 			_putchar('\n');
 		}
