3034327fe10782dcf82d01c9566d00f6d4ecc4b2
JoyNgaru <joywanjikum@gmail.com>
2023-03-27 06:44:51 -0700

    Task 6: display contents of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints out ll characters of string
+ * @str: string character on test
+ * return: 0
+ */
+void puts2(char *str)
+{
+	int content;
+
+	for (content = 0; str[content] != '\0'; content++)
+		if (content % 2 == 0)
+			_putchar(str[content]);
+	_putchar('\n');
+}
+
