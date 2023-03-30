563f0226e0faabd584d221c105459de4002489ad
JoyNgaru <joywanjikum@gmail.com>
2023-03-30 00:27:32 -0700

    Task 0: Concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,24 +2,25 @@
 /**
  * _strcat - function to join two strings
  * function ensures while src and dest are not at the null byte
- * and equates src to dest to join them 
+ * and equates src to dest to join them
  * @src: string 1
  * @dest: sring 2
  * Return: pointer to the dest
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	char *s = dest;
+
 	while (*dest != '\0')
 	{
-		dest ++;
+		dest++;
 	}
 	while (*src != '\0')
 	{
-		*dest=*src;
-		src ++;
-		*dest ++;
+		*dest = *src;
+		src++;
+		*dest++;
 	}
-	*dest= '\0';
+	*dest = '\0';
 	return (s);
 }
