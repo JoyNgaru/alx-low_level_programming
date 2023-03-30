f2ff356b8fae500678efe383c938c8e70cd3c44e
JoyNgaru <joywanjikum@gmail.com>
2023-03-30 05:23:50 -0700

    Task 6:  capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be captalized
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int x = 0, y;
+	char specialChar[13] = " \t\n,;.!?\"(){}";
+
+
+	for (x = 0, s[x] != '\0'; x++)
+	{
+		if (x == 0 && s[x] >= 'a' && s[x] <= 'z')
+			s[i] -= 32;
+
+		for (y = 0; y < 13; y++)
+		{
+			if (s[x] == specialChar[y])
+			{
+				if (s[x + 1] >= 'a' && s[x + 1] <= 'z')
+				{
+					s[x + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
