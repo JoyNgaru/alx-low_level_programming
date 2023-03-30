d8cbb997a8c2788c5fab21c88622300ff36bfa64
JoyNgaru <joywanjikum@gmail.com>
2023-03-30 02:55:14 -0700

    Task 5: changer lowrcase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - change small to caps
+ * @s: string to work on
+ * Return: s
+ */
+char *string_toupper(char *s)
+{
+	int x = 0;
+
+
+	while (*(s + x) >= 'a' && *(s + x) <= 'z')
+		*(s + x) -= 'a' - 'A';
+	x++
+}
+return (s);
+}
