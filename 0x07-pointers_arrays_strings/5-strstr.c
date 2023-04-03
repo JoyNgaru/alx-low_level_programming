4d88243d49d0b142a86685192c8b3767fc72f39c
JoyNgaru <joywanjikum@gmail.com>
2023-04-03 14:25:50 -0700

    Task 5: find the needle in the haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - find the first appearance of needle
+ * @needle: what we need to find
+ * @haystack: where we need to find the needle
+ * Return: location or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *dry = haystack;
+		char *shiny = needle;
+
+
+		while (*dry == *shiny && *shiny != '\0')
+		{
+			shiny++;
+			dry++
+		}
+		if (*shiny == '\0;)
+			return (haystack);
+	}
+	return (0);
+}
+
