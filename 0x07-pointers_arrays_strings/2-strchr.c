7e3747628632a8097e1f655acab54e9268456d30
JoyNgaru <joywanjikum@gmail.com>
2023-04-03 09:28:48 -0700

    Task2: locate a char in a str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - find a character in a string
+ * @s:the string in question
+ * @c: character to be found
+ * Return: first sighting the character
+ */
+char *_strchr(char *s, char c)
+{
+	if (c == '\0')
+	{
+		while (*s != c; s++)
+		{
+			return (s);
+		}
+		return (s);
+	}
+	return (NULL);
+}
+
