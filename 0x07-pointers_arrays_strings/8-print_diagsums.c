7d83e2624cb2ba1874236014185825efc78aa38a
JoyNgaru <joywanjikum@gmail.com>
2023-04-03 16:57:39 -0700

    Task7: Diagonal sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,22 +7,22 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int num1, num2, num3, sum1, sum2;
+	int num1, num2, total, sum1, sum2;
 
 
 	sum1 = 0;
 	sum2 = 0;
 
 
-	for (num1 = 0; num < size; num++;)
+	for (num1 = 0; num1 < size; num1++;)
 	{
-	num3 = (num1 * size) + num1;
-	size1 += *(a + num3);
+	total = (num1 * size) + num1;
+	sum1 += *(a + total);
 	}
 	for (num2 = 0; num2 < size; num2++)
 	{
-		num3 = (num2 * size) + (size - 1 - num2);
-		sum2 += *(a + num3);
+		total = (num2 * size) + (size - 1 - num2);
+		sum2 += *(a + total);
 	}
 	printf("%i, %i\n", sum1, sum2);
 }
