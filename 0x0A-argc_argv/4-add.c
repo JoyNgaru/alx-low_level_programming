9b20b23a250f5bf10e8219b61238678ed1929adb
JoyNgaru <joywanjikum@gmail.com>
2023-04-09 13:53:05 -0700

    program that adds arguments it receives.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
 #include "main.h"
 /**
- * main - function that outputs the sum of digits
- * @argc: argument counter
- * @argv: arguments being passed
- * Return: 0
+ * main - function to add two arguments
+ * @argc: Total number of arguments passed
+ * @argv: Arguments in the array
+ * Return: 0 if success,else,  1
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int digit1, digit2, sum = 0;
-	char flag;
+	int sum;
+	int i;
+	const char *arg;
+	const char *p;
 
-
-	if (argc < 2)
+	sum = 0;
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (digit1 = 1; argv[digit1]; digit1++)
-	{
-		digit2 = strtol(argv[digit1], &flag, 10);
-		if (*flag)
+		arg = argv[i];
+		for (p = arg; *p != '\0'; p++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(*p))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			sum += n;
-		}
+		sum += atoi(arg);
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
