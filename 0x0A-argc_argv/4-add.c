64a1bc63e832a83d35785999c3582ea788f56ecc
JoyNgaru <joywanjikum@gmail.com>
2023-04-09 13:25:59 -0700

    program that adds arguments it receives.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
 /**
  * main - function that outputs the sum of digits
  * @argc: argument counter
