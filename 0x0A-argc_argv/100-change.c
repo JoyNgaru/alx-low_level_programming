02e6fac3d86021c90c3f6a8543d39a410a35f662
JoyNgaru <joywanjikum@gmail.com>
2023-04-09 14:13:57 -0700

    program that gives put change.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,16 +1,18 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - outputs coins to get change
- * @argc: number of arguments passe
- * @argv: arguments being passed
- * Return: 0
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
-int main(int argc, char **argv)
-
-		int num, j, result;
+int main(int argc, char *argv[])
+{
+	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
@@ -40,6 +42,11 @@ int main(int argc, char **argv)
 	printf("%d\n", result);
 	return (0);
 }
+
+
+
+
+
 
 
 
