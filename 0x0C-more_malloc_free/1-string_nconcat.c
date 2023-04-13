5f9997a3e8068a7282e79378318af66ef02e18b5
JoyNgaru <joywanjikum@gmail.com>
2023-04-13 02:20:02 -0700

    Task1: Concatenate s1 and s2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - joins two strings
- * @s1: main string to receive s2 
- * @s2: string to be joined to s1
- * @n: amount of bytes for s2 to be joined to s1
- * Return: pointer to concatenated string in memory
- */
+* string_nconcat - joins two strings
+* @s1: main string to receive s2
+* @s2: string to be joined to s1
+* @n: amount of bytes for s2 to be joined to s1
+* Return: pointer to concatenated string in memory
+*/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -32,17 +32,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (pointer == NULL) /*If the function fails, it should return NULL */
 		return (NULL);
 
-	for (content1 = 0; content1 < length_s1; content1++) /* check for every value of s1 and increament it from 0 */
+	for (content1 = 0; content1 < length_s1; content1++)
+/* check for every value of s1 and increament it from 0 */
 	{
 		pointer[content1] = s1[content1];
 	}
 
-	for (content2 = 0; content2 < n; content2++, content1++) /* If n is greater or equal to the length of s2 then use the entire string s2 */
+	for (content2 = 0; content2 < n; content2++, content1++)
+/* If n >= to the length of s2 then use the entire string s2 */
 	{
 		pointer[content1] = s2[content2];
 	}
 
-	pointer[content1] = '\0'; /* End the process when we get to the null terminator */
+	pointer[content1] = '\0';
+/* End the process when we get to the null terminator */
 
 	return (pointer);
 }
