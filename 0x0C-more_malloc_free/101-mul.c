f3c6e3057505666f5c77c0f2b8b67ecb467a9691
JoyNgaru <joywanjikum@gmail.com>
2023-04-13 05:58:13 -0700

    Task 5: multiplication

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -60,7 +60,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *string1, *string2;
-	int length1, length2, new_length, i, mod, num1, num2, *multi_answer, a = 0;
+	int length1, length2, new_length, content, mod, num1, num2, *multi_answer, a = 0;
 
 	string1 = argv[1], string2 = argv[2];
 	if (argc != 3 || !is_digit(string1) || !is_digit(string2))
@@ -71,8 +71,8 @@ int main(int argc, char *argv[])
 	multi_answer = malloc(sizeof(int) * new_length);
 	if (!multi_answer)
 		return (1);
-	for (i = 0; i <= length1 + length2; i++)
-		multi_answer[i] = 0;
+	for (content = 0; content <= length1 + length2; content++)
+		multi_answer[content] = 0;
 	for (length1 = length1 - 1; length1 >= 0; length1--)
 	{
 		num1 = string1[length1] - '0';
@@ -89,10 +89,10 @@ int main(int argc, char *argv[])
 	}
 	for (i = 0; i < new_length - 1; i++)
 	{
-		if (multi_answer[i])
+		if (multi_answer[content])
 			a = 1;
 		if (a)
-			_putchar(multi_answer[i] + '0');
+			_putchar(multi_answer[content] + '0');
 	}
 	if (!a)
 		_putchar('0');
