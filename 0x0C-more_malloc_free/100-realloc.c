cc64418c7a77716d53f31ec249693d103b888dac
JoyNgaru <joywanjikum@gmail.com>
2023-04-13 03:23:28 -0700

    Task 4: reallocation

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_pointer;
 
-	new_pointer = NULL;
+	new_pointer = 0;
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
