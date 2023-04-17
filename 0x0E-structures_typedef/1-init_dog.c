172778cf531dd31ad97847817c93d5834c3c3106
JoyNgaru <joywanjikum@gmail.com>
2023-04-17 01:55:39 -0700

    initialize the values of a structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - function to set values of our dog
+ * @d: pointer to struct that will be used to init
+ * @name: pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: pointer to owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
