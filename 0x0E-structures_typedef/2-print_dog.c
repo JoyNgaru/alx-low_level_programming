41bca1bddc3d964b26724feeae303bd77100c7ca
JoyNgaru <joywanjikum@gmail.com>
2023-04-17 02:29:33 -0700

    output  details of the dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prototype function to output dog info
+ * @d: pointer to the dog's details
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (*d).name == NULL)
+		(*d).name = "(nil)";
+	if (*d).owner == NULL)
+		(*d).owner = "(nil)";
+
+	printf("The dog's name is: %s\n", d->name;
+	printf("The dog's age is: %f\n", d->age;
+	printf("The dog's owner is: %s\n", d->owner;
+
+}
