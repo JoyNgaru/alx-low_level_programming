49ae94b1918590519a4165cec46df9bbf69a7789
JoyNgaru <joywanjikum@gmail.com>
2023-04-17 11:02:56 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,13 +9,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog = malloc(sizeof(dog_t));
+	unsigned int dog_det, age_det, owner_det;
+	dog_t *dog_ptr;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+	for (dog_det = 0; name[dog_det]; dog_det++)
+		;
+	dog_det++;
+	dog->name = malloc(dog_det * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (owner_det = 0; owner_det < dog_det; owner_det++)
+		dog->name[owner_det] = name[owner_det];
 	dog->age = age;
-	dog->owner = owner;
-
+	for (age_det = 0; owner[age_det]; age_det++)
+		;
+	age_det++;
+	dog->owner = malloc(age_det * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (owner_det = 0; owner_det < age_det; owner_det++)
+		dog->owner[owner_det] = owner[owner_det];
 	return (dog);
 }
