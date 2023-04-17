9bb330d42baedd572fd43107cf2759c40b393f27
JoyNgaru <joywanjikum@gmail.com>
2023-04-17 02:41:45 -0700

    output  details of the dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,10 +10,10 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if ((*d).name == NULL)
-		(*d).name = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		(*d).owner = "(nil)";
+		d->owner = "(nil)";
 
 	printf("The dog's name is: %s\n", d->name;
 	printf("The dog's age is: %f\n", d->age;
