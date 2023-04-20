ce16e87006dc97075f99510a722a7001467f3e12
JoyNgaru <joywanjikum@gmail.com>
2023-04-20 06:04:30 -0700

    print a numbrs

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(arguments, int));
 
-		if (separator && count < n - 1)
+		f (separator && count < n - 1)
 			printf("%s", separator);
 		count++;
 	}
