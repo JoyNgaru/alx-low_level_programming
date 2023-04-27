e7fc80ab318e52a50ef808fbf54b0f380d03ecff
JoyNgaru <joywanjikum@gmail.com>
2023-04-27 13:49:55 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 #ifndef _PRINT_LIST
 #define _PRINT_LIST
-#define _FMS(x) ((x) ? (x) : ("(nil)"))/*x>0, if yes, nil, else returns x*/
-#endif
+#define _FMS(x) ((x) ? (x) : ("(nil)")) /* Format string */
+#endif /* _PRINT_LIST */
 
 /**
- * print_list - function to output the elemens of a list
- * @h: pointer to list_t
- * Return: amount of nodes in the singly list
+ * print_list - Prints all elements of a list_t list
+ * @h: Pointer to the singly-linked list
+ * Return: number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t content;
 	unsigned int len;
 
-	for (i = 0; h; i++)
+	for (content = 0; h; content++)
 	{
 		len = (h->str == NULL) ? (0) : (h->len);
 		printf("[%d] %s\n", len, _FMS(h->str));
 		h = h->next;
 	}
 
-	return (i);
+	return (content);
 }
