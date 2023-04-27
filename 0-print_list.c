c5a7d230f575fd869712dc8385b9c49532198b63
JoyNgaru <joywanjikum@gmail.com>
2023-04-27 13:39:53 -0700

    Task 0

--- 0-print_list.c
+++ 0-print_list.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
-#ifndef _PRINT_LIST
-#define _PRINT_LIST
-#define _FMS(x) ((x) ? (x) : ("(nil)")) /* Format string */
-#endif /* _PRINT_LIST */
-
 /**
- * print_list - Prints all elements of a list_t list
- * @h: Pointer to the singly-linked list
- * Return: number of nodes in the list
+ * print_list - a function that prints all the elements of  list_t listi
+ * @h: pointer to the lists_t list to  print
+ *
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
-	unsigned int len;
+	size_t content = 0;
 
-	for (i = 0; h; i++)
+	while (h)
 	{
-		len = (h->str == NULL) ? (0) : (h->len);
-		printf("[%d] %s\n", len, _FMS(h->str));
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		content++
 	}
 
-	return (i);
+	return (content);
 }
