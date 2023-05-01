ee6aca66636f693b956b59b42de70553b3743ccd
JoyNgaru <joywanjikum@gmail.com>
2023-05-01 01:08:59 -0700

    list all elements in a singly list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_listint - Output all elements in the list
+ * h - pointer to the first node's address
+ * Return: elements on the node
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t size;
+	
+	size = 0;
+	while (h)
+	{
+		printf("%d\n", h->elem);
+		h = h->next;
+		size++;
+	}
+	return (size);
