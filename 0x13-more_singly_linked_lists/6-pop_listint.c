9ae52faf2d59112ff553063182a5463857981457
JoyNgaru <joywanjikum@gmail.com>
2023-05-02 04:30:05 -0700

    free the list and return nodes data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * pop_listint - function to deletes the head node
+ * @head: pointer to the head pointer
+ * Return: the head node’s data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int node_data;
+
+	if (head == NULL)
+	       return (0);
+	if (*head == NULL)
+		return (0);
+
+	node_data = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (node_data);
+}
+
+
+
+
+
+
+
+
+
+}
