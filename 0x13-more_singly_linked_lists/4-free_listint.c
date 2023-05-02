a8be6ea6b1f550149e08312bc73877bd09c98d78
JoyNgaru <joywanjikum@gmail.com>
2023-05-02 10:06:42 -0700

    update

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,9 +7,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *last_node;
 
-	do {
+	while (head)
+	{
 		last_node = head->next;
-		free(head);
-		head = last_node;
-	} while (head);
+                free(head);
+                head = last_node;
+	}
 }
