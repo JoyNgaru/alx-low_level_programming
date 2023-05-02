4260e187e42fea7df9a6a7137120865478480f72
JoyNgaru <joywanjikum@gmail.com>
2023-05-02 06:25:52 -0700

    Total sum of n in a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - output sum of all data on the list
+ * @head: pointer to the head
+ * Return: sum of the data
+ */
+int sum_listint(listint_t *head)
+{
+	int total_sum;
+	listint_t *temp = head;
+
+	total_sum = 0;
+	do  {
+		total_sum += temo->n;
+		temp = temp->next;
+	} while (temp);
+
+	return (sum);
+}
+
+
