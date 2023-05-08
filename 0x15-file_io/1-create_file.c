e8e085cb0e7d85f1751521174e23c489290a8980
JoyNgaru <joywanjikum@gmail.com>
2023-05-08 05:02:08 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,33 +6,43 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
- * create_file - Function that creates a file.
- * @filename: Name of the file to create.
- * @text_content: NULL terminated string to write to the file.
+ * create_file -A function that creates a file.
+ * @filename: New filename
+ * @text_content: content to be written on the file.
+ * This code defines a function called create_file
+ * that takes two arguments: filename and text_content.
+ * The function creates a file with the specified filename
+ * and writes the text_content string to the file. 
+ * If the file already exists, its content is truncated 
+ * and overwritten with the new content
+ * 
  * Return: On success, 1, otherwise, -1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, length;
-	ssize_t n_write;
-
+	int file_des, count;
+	ssize_t content_write;
+/*if file name is non-existent, exit*/
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	/*create a  file, if not there and write/ trunc it*/
+	file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_des == -1)/*on failure, exit*/
 		return (-1);
-	if (text_content != NULL)
+	if (text_content != NULL)/*add content*/
 	{
-		length = 0;
-		while (*(text_content + length) != '\0')
-			length++;
-		n_write = write(fd, text_content, length);
-		if (n_write == -1)
+		count = 0;
+		/*if the ccounter is not at the null byte of the string, write content*/
+		while (*(text_content + count) != '\0')
+			count++;
+		content_write = write(file_des, text_content, count);
+		/*if the writing fails, exit and give a message*/
+		if (content_write == -1)
 		{
 			write(1, "fails\n", 6);
 			return (-1);
 		}
 	}
-	close(fd);
+	close(file_des);
 	return (1);
 }
